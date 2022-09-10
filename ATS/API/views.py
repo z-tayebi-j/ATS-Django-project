aeb76ae3f5ea82977e5c335f11fc467a43aeb30f
Amin Ehsanimehr <aminehsanimehr14@gmail.com>
2022-09-10 23:28:33 +0430

    imports added

--- ATS/API/views.py
+++ ATS/API/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+
+from .models import Applicant, Interview, Interviewer, Feedback
+from .serializers import ApplicantSerializer, InterviewSerializer, InterviewerSerializer, FeedbackSerializer
 
 # Create your views here.
