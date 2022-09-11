bab64f3b2a47af40ff5304c39b1cc4f1b004c63a
Amin Ehsanimehr <aminehsanimehr14@gmail.com>
2022-09-11 19:47:40 +0430

    You can now post an interviewer

--- ATS/API/urls.py
+++ ATS/API/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 from rest_framework.authtoken import views
 
-urlpatterns = []
+from .views import CreateInterviewerAPIView
+
+urlpatterns = [
+    path('interviewer_signup/', CreateInterviewerAPIView.as_view()),
+    # path('edit_applicant/<int:pk>/', UpdateApplicantView.as_view())
+]
--- ATS/API/views.py
+++ ATS/API/views.py
@@ -5,4 +5,9 @@ from rest_framework.response import Response
 from .models import Applicant, Interview, Interviewer, Feedback
 from .serializers import ApplicantSerializer, InterviewSerializer, InterviewerSerializer, FeedbackSerializer
 
-# Create your views here.
+
+class CreateInterviewerAPIView(generics.CreateAPIView):
+    authentication_classes = []
+    permission_classes = []
+    queryset = Interviewer
+    serializer_class = InterviewerSerializer
