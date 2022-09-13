9f7b7dc4f3ca448d2dfba144bc4d907e50694cf9
Amin Ehsanimehr <aminehsanimehr14@gmail.com>
2022-09-13 21:59:48 +0430

    some bugs fixed

--- ATS/API/admin.py
+++ ATS/API/admin.py
@@ -3,6 +3,4 @@ from django.contrib import admin
 from .models import Interviewer, Interview, Applicant, Feedback
 
 admin.site.register(Interview)
-admin.site.register(Interviewer)
-admin.site.register(Feedback)
 admin.site.register(Applicant)
